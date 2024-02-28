c0b93fafc4696102aaebc4f48bbc64a293a20f46
user <ilasmyga26@gmail.com>
2024-02-29 01:25:15 +0300

    last commit

---
+++ src/Exercise1.java
@@ -0,0 +1,10 @@
+public class Exercise1 {
+    public static void main(String [] args) {
+        int i1 = 5, i2 = 11;
+        double d1 = 5.5, d2 = 1.3;
+        long l = 20l;
+        double result;
+        result = i2 / d1 + d2 % i1 - l;
+        System.out.println(result);
+    }
+}
--- src/Exercise2.java
+++ src/Exercise2.java
@@ -1,1 +1,7 @@
-
+public class Exercise2 {
+    public static void main(String [] args) {
+        int a = 5, inca = a-- - --a + ++a + a++ + a;
+        int b = 8, incb = ++b - b++ + ++b - --b;;
+        System.out.println(inca + "\n" + incb);
+    }
+}
